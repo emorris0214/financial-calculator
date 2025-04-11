9d778e06f0f5a71d1b581a18d86bb922fff4c597
unknown <Enoch Morris>
2025-04-11 16:29:44 -0400

    Finished first section of if/else if/else statements on Workshop1

--- FinancialCalculators/src/main/java/com/pluralsight/FinancialCalculators.java
+++ FinancialCalculators/src/main/java/com/pluralsight/FinancialCalculators.java
@@ -1,14 +1,28 @@
 package com.pluralsight;
 import java.util.Scanner;
+
 public class FinancialCalculators {
     public static void main(String[] args) {
-        //calling scanner
+        // calling scanner
         Scanner keyboard = new Scanner(System.in);
 
-        //introduction and call for input
+        // introduction and call for input
         System.out.println("Welcome to E's Calc-ies!");
-        System.out.println("Which calculator would you like to use?");
+        System.out.print("Which calculator would you like to use? (1, 2, or 3) ");
         int calc = keyboard.nextInt();
+
+        // printing a statement when app is given an option
+        if (calc == 1){
+            System.out.println("Thanks for using our Mortgage Calculator");
+        } else if (calc == 2){
+            System.out.println("Thanks for using our CD Future Worth Calculator");
+        } else if (calc == 3){
+            System.out.println("Thanks for using our Present Value Annuity Calculator");
+        } else {
+            System.out.println("The option you've chosen is invalid. Restart application.");
+        }
+        System.out.println("What is your ...");
+
 
 
     }
